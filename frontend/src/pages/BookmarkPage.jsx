a18a596217515d033f4ff6619026a9d4c36ba907
DanaiWee <Danai_wee@hotmail.com>
2025-08-27 10:52:15 +0700

    Implement getBookmarkPosts

--- frontend/src/pages/BookmarkPage.jsx
+++ frontend/src/pages/BookmarkPage.jsx
@@ -1,21 +1,18 @@
 import { FaArrowLeft } from "react-icons/fa";
-import { POSTS } from "../constants";
 import { Link } from "react-router-dom";
 import Posts from "../compoenents/Posts";
+import { usePostStore } from "../stores/usePostStore.js";
+import { useEffect } from "react";
 
 const BookmarkPage = () => {
-  const isLoading = false;
-  const isFetching = false;
-  const user = [];
+  const { isFetchingPosts, fetchPosts, posts } = usePostStore();
 
-  const posts = POSTS;
+  useEffect(() => {
+    fetchPosts("Bookmarks");
+  }, [fetchPosts]);
+
   return (
     <main className='container'>
-      {(isLoading || isFetching) && <ProfileHeaderSkeleton />}
-      {!isLoading && !isFetching && !user && (
-        <p className='text-center text-lg mt-20'>User not found</p>
-      )}
-
       <div className='flex gap-10 px-4 py-2 items-center mt-1 border-b border-gray-700'>
         <Link to='/'>
           <FaArrowLeft className='size-4' />
@@ -23,11 +20,11 @@ const BookmarkPage = () => {
 
         <div className='flex flex-col justify-center'>
           <p className='font-bold text-lg'>Bookmark</p>
-          <p className='text-sm text-slate-500'>{posts.length} posts</p>
+          <p className='text-sm text-slate-500'>{posts?.length} posts</p>
         </div>
       </div>
 
-      <Posts feedType='posts' />
+      <Posts posts={posts} isFetchingPosts={isFetchingPosts} />
     </main>
   );
 };
