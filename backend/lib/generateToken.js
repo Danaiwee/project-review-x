8c0f5df421a95e8aef5f09de4aba35a4936964f9
DanaiWee <Danai_wee@hotmail.com>
2025-08-28 12:10:13 +0700

    Edit cookies property

--- backend/lib/generateToken.js
+++ backend/lib/generateToken.js
@@ -8,7 +8,7 @@ export const generateTokenAndSetCookie = (userId, res) => {
   res.cookie("accessToken", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "None",
     secure: process.env.NODE_ENV === "production", //send only on https
   });
 };
