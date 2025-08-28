714d40d895b40b89208b05cb6c33b4c8e042b80c
DanaiWee <Danai_wee@hotmail.com>
2025-08-28 10:27:48 +0700

    Deploy configuration for vercel

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 
 import connectDB from "./lib/connectDB.js";
 import logger from "./lib/logger.js";
@@ -16,29 +15,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/notifications", notificationRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   logger.info(`Server is running on PORT ${PORT}`);
